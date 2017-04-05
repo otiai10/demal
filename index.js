1a650c4af91462abb71dcbbf79b21bd271d423c9
Hiromu OCHIAI <otiai10@gmail.com>
2017-04-05 10:04:21 +0900

    Refactor code style

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 module.exports = (function() {
+  var delim = ',',
+      joint = '|',
+      depth = '.';
   var Demal = function(raw, dict) {
     this.raw  = raw  || '';
     this.dict = dict || {};
   };
   Demal.parse = function(str) {
-    const dict = str.split(',')
-    .map(function(e) { return e.split('|'); })
+    const dict = str.split(delim)
+    .map(function(e) { return e.split(joint); })
     .reduce(function(self, kvp) {
       if (kvp.length < 2) return self;
       var key = kvp[0], val = kvp[1];
-      return Demal._recursiveSet(self, key.split('.'), val);
+      return Demal._recursiveSet(self, key.split(depth), val);
     }, {});
     return new Demal(str, dict);
   };
@@ -24,8 +27,8 @@ module.exports = (function() {
   Demal._primitiveCast = function(val) {
     // TODO: refactor
     switch(val) {
-    case "true": return  true;
-    case "false": return false;
+    case 'true': return  true;
+    case 'false': return false;
     }
     if (val.match(/[0-9]+\.?[0-9]*/)) return parseFloat(val);
     return val;
@@ -33,14 +36,14 @@ module.exports = (function() {
   Demal.serialize = function(dict) {
     return Object.keys(dict).map(function(key) {
       return Demal._getString(key, dict[key]);
-    }).join(',');
+    }).join(delim);
   };
   Demal._getString = function(key, value, prefix) {
     if (typeof value == 'object') return Object.keys(value).map(function(k) {
       return Demal._getString(k, value[k], key);
-    }).join(',');
-    var k = prefix ? [prefix, key].join('.') : key;
-    return [k, value].join('|');
+    }).join(delim);
+    var k = prefix ? [prefix, key].join(depth) : key;
+    return [k, value].join(joint);
   };
   Demal.prototype.json = function() {
     return this.dict;
