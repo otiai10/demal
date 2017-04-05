18cb926d75e161ceb11a5959de11c04abe83db57
Hiromu OCHIAI <otiai10@gmail.com>
2017-04-05 10:02:21 +0900

    Fix test suit name

--- __tests__/index-spec.js
+++ __tests__/index-spec.js
@@ -2,7 +2,7 @@ jest.unmock('../index.js');
 var Demal = require('../');
 
 describe('Demal', () => {
-  describe('parse', () => {
+  describe('static parse', () => {
     it('should parse demal string', () => {
       const str = `u|http://google.com,s.w|520,s.h|338,o.l|0,o.t|0,z|1`;
       const demal = Demal.parse(str);
